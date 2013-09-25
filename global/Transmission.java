b2edcb7aaf7b13de8d5f87ed7857fac8cef106c3
John Mather <NextDesign1@users.noreply.github.com>
2013-09-24 21:19:24 -0400

    Setting up projects

---
+++ global/Transmission.java
@@ -0,0 +1,20 @@
+import java.util.zip.*;
+
+public class Transmission
+{
+	public void compress()
+	{
+	}
+	
+	public void decompress()
+	{
+	}
+	
+	public void encrypt()
+	{
+	}
+	
+	public void decrypt()
+	{
+	}
+}
---
+++ server/src/com/utoronto/csc301/ultrasound/Server.java
@@ -0,0 +1,12 @@
+package com.utoronto.csc301.ultrasound;
+
+public class Server 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	}
+}
