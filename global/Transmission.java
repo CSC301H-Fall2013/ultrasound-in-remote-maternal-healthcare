b4378be5332a085470a6e3759837d3a40986a983
John Mather <NextDesign1@users.noreply.github.com>
2013-09-25 21:31:01 -0400

    Udated user stories

--- global/Transmission.java
+++ global/Transmission.java
@@ -1,20 +1,83 @@
+package com.utoronto.csc301.ultrasound;
+
+import java.io.*;
 import java.util.zip.*;
+
+import javax.crypto.*;
 
 public class Transmission
 {
-	public void compress()
+	public byte[] compress(byte toCompress[])
+	{
+		try 
+		{
+			Deflater compressor = new Deflater(Deflater.BEST_COMPRESSION);
+			
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			DeflaterOutputStream compressedStream = new DeflaterOutputStream(stream, compressor);
+			
+			compressedStream.write(toCompress);
+			compressedStream.close();
+			
+			stream.flush();
+			
+			float ratio = (float)compressor.getBytesWritten() / (float)compressor.getBytesRead();
+			
+			System.out.format("Compressed %d bytes to %d bytes. Ratio: %.2f\n", compressor.getBytesRead(), compressor.getBytesWritten(), ratio);
+			
+			compressor.end();
+			
+			return stream.toByteArray();
+		} 
+		catch (UnsupportedEncodingException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public byte[] decompress(byte toDecompress[])
+	{
+		try 
+		{
+			Inflater decompressor = new Inflater();
+			
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			InflaterOutputStream decompressedStream = new InflaterOutputStream(stream, decompressor);
+			
+			decompressedStream.write(toDecompress);
+			decompressedStream.close();
+			
+			stream.flush();
+			
+			float ratio = (float)decompressor.getBytesWritten() / (float)decompressor.getBytesRead();
+			
+			System.out.format("Decompressed %d bytes to %d bytes. Ratio: %.2f\n", decompressor.getBytesRead(), decompressor.getBytesWritten(), ratio);
+			
+			return stream.toByteArray();
+		} 
+		catch (UnsupportedEncodingException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public void encrypt(byte toEncrypt[])
 	{
 	}
 	
-	public void decompress()
-	{
-	}
-	
-	public void encrypt()
-	{
-	}
-	
-	public void decrypt()
+	public void decrypt(byte toDecrypt[])
 	{
 	}
 }
--- server/src/com/utoronto/csc301/ultrasound/Server.java
+++ server/src/com/utoronto/csc301/ultrasound/Server.java
@@ -1,5 +1,10 @@
 package com.utoronto.csc301.ultrasound;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+import com.utoronto.csc301.ultrasound.Transmission;
+
 public class Server 
 {
 	/**
@@ -7,6 +12,50 @@ public class Server
 	 */
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		testCompression();
+	}
+	
+	private static void testCompression()
+	{
+		String data = "blahblahblah??blahblahblah??blahblahblah??blahblahblah??blahblahblah??blahblahblah??";
+		byte[] input = {};
+		
+		try 
+		{
+			input = data.getBytes("UTF-8");
+		} 
+		catch (UnsupportedEncodingException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		Transmission t = new Transmission();
+		
+		byte compressed[] = t.compress(input);
+		byte decompressed[] = t.decompress(compressed);
+		
+		if (!Arrays.equals(input, decompressed))
+			System.err.println("Error with compression!");
+		
+		StringBuilder sb = new StringBuilder();
+		
+	    for (byte b : input)
+	        sb.append(String.format("%02X ", b));
+	    
+	    System.out.println(sb.toString());
+	    
+	    sb = new StringBuilder();
+		
+	    for (byte b : compressed)
+	        sb.append(String.format("%02X ", b));
+	    
+	    System.out.println(sb.toString());
+	    
+	    sb = new StringBuilder();
+		
+	    for (byte b : decompressed)
+	        sb.append(String.format("%02X ", b));
+	    
+	    System.out.println(sb.toString());
 	}
 }
