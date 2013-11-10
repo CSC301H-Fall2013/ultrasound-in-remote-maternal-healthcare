68b0d015bad6c052432f03c8dcca1f60f6791ee1
g2vinay <vinay.gera@mail.utoronto.ca>
2013-11-09 22:25:13 -0500

    Update auth.php

--- AZDp1lgcE7ijQ0p6/controllers/auth.php
+++ AZDp1lgcE7ijQ0p6/controllers/auth.php
@@ -70,7 +70,7 @@ class Auth extends CI_Controller {
 			echo "true";
 			$remember = (bool) $this->input->post('remember');
 
-			/f ($this->ion_auth->login($this->input->post('identity'), $this->input->post('password'), $remember))
+			if ($this->ion_auth->login($this->input->post('identity'), $this->input->post('password'), $remember))
 			{
 				//if the login is successful
 				//redirect them back to the home page
@@ -101,7 +101,7 @@ class Auth extends CI_Controller {
 		//		'type' => 'password',
 		//	);
 		
-			$this->load->view('welcome_message');
+			$this->load->view('auth/login');
 			
 		}
 	}
