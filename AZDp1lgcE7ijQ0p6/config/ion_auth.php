1fd22b3ccb1a57445e05aed4735e836fb50976b9
John Mather <NextDesign1@users.noreply.github.com>
2013-11-10 00:42:28 -0500

    Specifying db schema

--- AZDp1lgcE7ijQ0p6/config/ion_auth.php
+++ AZDp1lgcE7ijQ0p6/config/ion_auth.php
@@ -31,10 +31,10 @@ $config['collections']['login_attempts'] = 'login_attempts';
 | -------------------------------------------------------------------------
 | Database table names.
 */
-$config['tables']['users']           = 'users';
-$config['tables']['groups']          = 'groups';
-$config['tables']['users_groups']    = 'users_groups';
-$config['tables']['login_attempts']  = 'login_attempts';
+$config['tables']['users']           = 'ultrasound.users';
+$config['tables']['groups']          = 'ultrasound.groups';
+$config['tables']['users_groups']    = 'ultrasound.users_groups';
+$config['tables']['login_attempts']  = 'ultrasound.login_attempts';
 
 /*
  | Users table column and Group table column you want to join WITH.
