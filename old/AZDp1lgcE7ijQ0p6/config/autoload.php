e37ea64229a69cef71df10984ef29281fe49490f
John Mather <NextDesign1@users.noreply.github.com>
2013-11-10 12:53:24 -0500

    Updating

--- old/AZDp1lgcE7ijQ0p6/config/autoload.php
+++ old/AZDp1lgcE7ijQ0p6/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session', 'email');
+$autoload['libraries'] = array('database', 'session', 'ion_auth', 'email');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database', 'session', 'email');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('language', 'url', 'form');
+$autoload['helper'] = array('language', 'url', 'form',);
 
 
 /*
