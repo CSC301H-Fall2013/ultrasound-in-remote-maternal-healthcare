fd329dd3870c537fa721b3478e19879c96f401c4
g2vinay <vinay.gera@mail.utoronto.ca>
2013-11-10 12:14:41 -0500

    Update auth.php

--- AZDp1lgcE7ijQ0p6/controllers/auth.php
+++ AZDp1lgcE7ijQ0p6/controllers/auth.php
@@ -232,8 +232,9 @@ class Auth extends CI_Controller {
 			}
 			else
 			{
-				//$this->session->set_flashdata('message', $this->ion_auth->errors());
-				redirect("auth/forgot_password", 'refresh');
+				$this->session->set_flashdata('message', $this->ion_auth->errors());
+				//redirect("auth/forgot_password", 'refresh');
+				$this->load->view('auth/forgot_password');
 			}
 			
 		}
