49fb7c280b7c46c288187c80c8bb719d9fa88105
John Mather <NextDesign1@users.noreply.github.com>
2013-11-11 22:39:16 -0500

    Increasing security by renaming root folders

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'system';
+	$system_path = 'sw2sw8castUh7pha';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application';
+	$application_folder = 'a5c74ya2eJeuqJs7';
 
 /*
  * --------------------------------------------------------------------
