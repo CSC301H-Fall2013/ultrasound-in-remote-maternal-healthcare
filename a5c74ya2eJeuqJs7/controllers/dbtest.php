bc664646aa24be8b3bb24323aa2ddea29c8ef208
g2vinay <vinay.gera@mail.utoronto.ca>
2013-11-12 18:10:25 -0500

    Update dbtest.php

--- a5c74ya2eJeuqJs7/controllers/dbtest.php
+++ a5c74ya2eJeuqJs7/controllers/dbtest.php
@@ -50,6 +50,30 @@ class Dbtest extends CI_Controller
 		}
 	}
 	
+	function insertPatientMed(){
+		if (isset($_REQUEST["pid"]) && isset($_REQUEST["fcomments"])
+				&& isset($_REQUEST["img"]) && isset($_REQUEST["gest"])
+				&& isset($_REQUEST["isbleed"]) && isset($_REQUEST["preb"])
+				&& isset($_REQUEST["diamFet"]) && isset($_REQUEST["diamot"])
+				&& isset($_REQUEST["fseen"])){
+			$this->load->database();
+			$this->load->model("Patients");
+			$pid = $_REQUEST["pid"];
+			$fcomments =  $_REQUEST["fcomments"];
+			$img = $_REQUEST["img"];
+			$gest = $_REQUEST["gest"];
+			$isbleed = $_REQUEST["isbleed"];
+			$preb =  $_REQUEST["preb"];
+			$diamFet =  $_REQUEST["diamFet"];
+			$diamot =  $_REQUEST["diamot"];
+			$fseen =  $_REQUEST["fseen"];
+			echo $this->Patients->insert_Patient_Med($pid, $fcomments, $img, $preb, $gest, $isbleed, $diamFet, $diamot, $fseen);
+		} else {
+			echo "nothing set!";
+		}
+	}
+	
+	
 		
 }
 
