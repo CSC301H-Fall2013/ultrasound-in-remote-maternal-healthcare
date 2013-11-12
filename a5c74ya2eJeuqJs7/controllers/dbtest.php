807bb167fc060fd414881d67369c37b3490dfdef
Vinay Gera <g2vinay@users.noreply.github.com>
2013-11-12 12:00:26 -0500

    Update db controllers.

--- a5c74ya2eJeuqJs7/controllers/dbtest.php
+++ a5c74ya2eJeuqJs7/controllers/dbtest.php
@@ -21,12 +21,14 @@ class Dbtest extends CI_Controller
 	}
 	
 	function callPatientCheck(){
-		if (isset($_REQUEST["firstName"]) && isset($_REQUEST["lastName"])) {
+		if (isset($_REQUEST["firstName"]) && isset($_REQUEST["lastName"])
+				&& isset($_REQUEST["date"])) {
 			$this->load->database();
 			$this->load->model("Patients");
 			$first = $_REQUEST["firstName"];
 			$last =  $_REQUEST["lastName"];
-			echo $this->Patients->check_Patient($first, $last);
+			$dat = $_REQUEST["date"];
+			echo $this->Patients->check_Patient($first, $last, $dat);
 		} else {
 			echo "nothing set!";
 		}
--- a5c74ya2eJeuqJs7/models/Patients.php
+++ a5c74ya2eJeuqJs7/models/Patients.php
@@ -12,8 +12,7 @@ class Patients extends CI_Model
 		$query = $this->db->query("SELECT * FROM ultrasound.Patients");
 		return $query->result(); }
 		
-	function check_Patient($first, $last){
-		$dat = "03 June 2013";
+	function check_Patient($first, $last, $dat){
 		$query = $this->db->query("DECLARE @p_date DATE  SET @p_date = CONVERT( DATE, '$dat')  SELECT * FROM ultrasound.Patients WHERE FirstName = '$first' 
 		AND LastName = '$last' AND Birthdate = @p_date");
 		if (count($query->result()) > 0){
