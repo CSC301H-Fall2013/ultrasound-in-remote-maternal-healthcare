8beb8d77afcbaf228ca9dda8fea25615757bf3f6
g2vinay <vinay.gera@mail.utoronto.ca>
2013-11-12 20:34:43 -0500

    Update Patients.php

--- a5c74ya2eJeuqJs7/models/Patients.php
+++ a5c74ya2eJeuqJs7/models/Patients.php
@@ -52,7 +52,7 @@ class Patients extends CI_Model
 			$floatmot = floatval($diamot);
 			
 		//$this->db->insert("ultrasound.Records", $da);
-		echo $this->db->query("DECLARE @image varbinary(max) SET @image = CONVERT(varbinary(max),'$binary')
+		$this->db->query("DECLARE @image varbinary(max) SET @image = CONVERT(varbinary(max),'$binary')
 						INSERT INTO ultrasound.Records (PID, Date, FieldworkerComments, IMGUltrasound,
 						Prebirth, Gestation, IsBleeding, DiameterFetalHead, DiameterMotherHip, FieldworkerSeen)
 						VALUES ('$intpid', '$curdat', '$fcomments', @image , '$preBirth',  '$intgest',
