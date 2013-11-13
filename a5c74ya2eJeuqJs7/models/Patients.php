8f0c0ad17bc0bb38ff613de146c16914a4997ada
g2vinay <vinay.gera@mail.utoronto.ca>
2013-11-12 19:13:08 -0500

    Update Patients.php

--- a5c74ya2eJeuqJs7/models/Patients.php
+++ a5c74ya2eJeuqJs7/models/Patients.php
@@ -41,15 +41,15 @@ class Patients extends CI_Model
 	
 	function insert_Patient_Med($pid, $fcomments, $img, $preb, $gest, $isbleed, $diamfet, $diamot, $fseen){
 		$da = array(
-			'PID' => $pid,
-			'Date' => date('Y/m/d H:i:s'),
-			'FieldworkerComments' => $fcomments,
+			'PID' => intval("$pid"),
+			'Date' => date("Y/m/d H:i:s"),
+			'FieldworkerComments' => "$fcomments",
 			'IMGUltrasound' => $img,
 			'Prebirth' => True,
-			'Gestation' => intval($gest),
+			'Gestation' => intval("$gest"),
 			'IsBleeding' => True,
-			'DiameterFetalHead' => $diamfet,
-			'DiameterMotherHip' => $diamot,
+			'DiameterFetalHead' => floatval("$diamfet"),
+			'DiameterMotherHip' => floatval("$diamot"),
 			'FieldworkerSeen' => False);
 		$this->db->insert('ultrasound.Records', $da);
 		$out = array( 'result' => 1);
