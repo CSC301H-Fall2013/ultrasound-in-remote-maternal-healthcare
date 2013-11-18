394ab6d6ea7812556835b19a35e5a5ab587bcddd
g2vinay <vinay.gera@mail.utoronto.ca>
2013-11-18 00:07:58 -0500

    Update Patients.php

--- a5c74ya2eJeuqJs7/models/Patients.php
+++ a5c74ya2eJeuqJs7/models/Patients.php
@@ -54,9 +54,9 @@ class Patients extends CI_Model
 			
 		//$this->db->insert("ultrasound.Records", $da);
 		//"DECLARE @image varbinary(max) SET @image = CONVERT(varbinary(max),'$newbinary')
-		$this->db->query("INSERT INTO ultrasound.Records (PID, Date, FieldworkerComments, IMGUltrasound
+		$this->db->query("INSERT INTO ultrasound.Records (PID, Date, FieldworkerComments,
 						Prebirth, Gestation, IsBleeding, DiameterFetalHead, DiameterMotherHip, FieldworkerSeen)
-						VALUES ('$intpid', '$curdat', '$fcomments', '$newbinary', '$preBirth', '$intgest',
+						VALUES ('$intpid', '$curdat', '$fcomments', '$preBirth', '$intgest',
 						'$isBleeding', '$floatfet', '$floatmot', '$preBirth')");
 		$out = array( "result" => 1);
 		return json_encode($out);
