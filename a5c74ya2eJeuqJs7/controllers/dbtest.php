c9a53bcf5448e669a79fc20f354c007a694dd65f
g2vinay <vinay.gera@mail.utoronto.ca>
2013-11-20 23:36:10 -0500

    Update dbtest.php

--- a5c74ya2eJeuqJs7/controllers/dbtest.php
+++ a5c74ya2eJeuqJs7/controllers/dbtest.php
@@ -88,7 +88,7 @@ class Dbtest extends CI_Controller
     		if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
     			/***  get the image info. ***/
     			
-    			$compressed = $this->compress($file_path, basename( $_FILES['uploaded_file']['name']), 60);
+    			$compressed = $this->compress($file_path, basename( $_FILES['uploaded_file']['name']), 90);
     			$size = getimagesize($compressed);
     			/*** assign our variables ***/
     			$type = $size['mime'];
